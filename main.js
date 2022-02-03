28a9cfcc2c2bd724785a6cd450b7480bf34e0fad
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2022-02-03 18:55:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -35,3 +35,56 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speak_prediction_1+speak_prediction_2);
     synth.speak(utterThis);
 }
+
+function check()
+{
+    img = document.getElementById("captured-image")
+    classifier.classify(img, gotResults)
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        document.getElementById("predction_1").innerHTML = prediction_1;
+        document.getElementById("predction_2").innerHTML = prediction_2;
+        speak();
+
+        if(prediction_1 == "Peace")
+        {
+            document.getElementById("emoji_1").innerHTML = "&#9996;";
+        }
+
+        if(prediction_2 == "Peace")
+        {
+            document.getElementById("emoji_2").innerHTML = "&#9996;";
+        }
+
+        if(prediction_1 == "High Five")
+        {
+            document.getElementById("emoji_1").innerHTML = "&#9995;";
+        }
+
+        if(prediction_2 == "High Five")
+        {
+            document.getElementById("emoji_2").innerHTML = "&#9995;";
+        }
+
+        if(prediction_1 == "Thumbs Up")
+        {
+            document.getElementById("emoji_1").innerHTML = "&#128077;";
+        }
+
+        if(prediction_2 == "Thumbs Up")
+        {
+            document.getElementById("emoji_2").innerHTML = "&#128077;";
+        }
+    }
+}
